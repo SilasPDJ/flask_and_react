1af171d5a41bd23b12f8b51b468c0071238ac486
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-08-31 12:26:54 -0300

    update comptPicker and add ModalDiv

---
+++ frontend/src/components/layout/ComptPicker.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker, YearCalendar, MonthCalendar } from '@mui/x-date-pickers';
+
+import dayjs from 'dayjs';
+import ptBr from 'dayjs/locale/pt-br'; // Import Portuguese locale data
+import ModalDiv from './ModalDiv';
+
+dayjs.locale(ptBr); // Set the locale
+
+export default function ComptPicker({ referenceDate }) {
+  return (
+    <ModalDiv openModalTitle={'Competência'} description="Selecione a competência" fontColor={'white'}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <hr />
+        <MonthCalendar
+          referenceDate={referenceDate}
+        />
+        <hr />
+        <YearCalendar
+
+          yearsPerRow={5}
+          sx={{
+            // gap: 2,
+            // width: 100
+            borderRadius: 2,
+            height: 250,
+
+          }}
+          referenceDate={referenceDate}
+        />
+        <hr />
+      </LocalizationProvider>
+    </ModalDiv>
+  );
+}
--- frontend/src/components/pages/competenciasPage.js
+++ frontend/src/components/pages/competenciasPage.js
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import styles from './competencias.module.css'
-import useFetch from '../layout/Form/hooks/useFetch';
-import { Button } from '@mui/material';
-import sendingData from '../layout/Form/helpers/postData';
+
 // import getDataWithQueryParameters from '../layout/Form/hooks/getDataWithQueryParams';
 import useFetchWithPathParams from '../layout/Form/hooks/useFetchWitPathParams';
 import MultiForm from '../layout/Form/MultiForm';
 import useFetchQuery from '../layout/Form/hooks/sql/useFetchQuery';
 import getSortedDataBasedOnArray from '../helpers/getSortedDataBasedOnArray';
+import ComptPicker from '../layout/ComptPicker';
 
 export default function CompetenciasPage() {
   // const [datasByParameter, setDatasByParameter] = useFetch('cadastro_competencias');
@@ -20,11 +19,15 @@ export default function CompetenciasPage() {
 
   const razaoSocialData = comptData.map(element => element['razao_social']);
   const ignoredKeys = ['razao_social']
+  // const [startDate, setStartDate] = useState(new Date());
 
   return (
     <>
-      competencias page
+      <div id={styles.comptDiv}>
+        
+        <ComptPicker />
 
+      </div>
       <MultiForm formDataArray={comptData} setFormDataArray={comptSetData} ignoredKeysArray={ignoredKeys} titleArray={razaoSocialData} apiUrlPostUpdate={urlUpdate} />
       {/* Necessário mudar para enviar */}
     </>
