d57df45187e5b2e61489e1dc67c5637616ca8961
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2023-09-01 19:12:42 -0300

    add FormGenerator.js componenet

--- frontend/src/components/layout/CreateEmpresa.js
+++ frontend/src/components/layout/CreateEmpresa.js
@@ -4,17 +4,17 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker, YearCalendar, MonthCalendar } from '@mui/x-date-pickers';
 import useFetchWithPathParams from './Form/hooks/useFetchWitPathParams';
 import useFetch from './Form/hooks/useFetch';
+import FormGenerator from './Form/FormGenerator';
 
 import dayjs from 'dayjs';
 import ModalDiv from './ModalDiv';
 
 export default function CreateEmpresa() {
-  const [template, setFormRef] = useFetch('sql/insert/new_client')
   return (
     <ModalDiv openModalTitle="CRIAR NOVO CLIENTE" fontColor={'white'}>
       {/* Use the ref to target the HTML element */}
       {/* <div dangerouslySetInnerHTML={{ __html: formRef }}></div> */}
-      <form dangerouslySetInnerHTML={{ __html: template['html'] }} />
+      <FormGenerator urlGetForm={'sql/insert/new_client'} />
 
     </ModalDiv>)
 }
---
+++ frontend/src/components/layout/Form/FormGenerator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './FormGenerator.module.css'
+import useFetch from './hooks/useFetch';
+
+
+export default function FormGenerator({ urlGetForm }) {
+  const [template, setFormRef] = useFetch(urlGetForm)
+
+
+  return (
+    <div className={styles.form_generated}>
+      <form dangerouslySetInnerHTML={{ __html: template['html'] }} />
+    </div>
+  )
+}
